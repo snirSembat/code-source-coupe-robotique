607b744dd3f2454d40450e78bd4b8a43daebd28d
toto00003 <150148693+toto00003@users.noreply.github.com>
2024-03-26 15:51:04 +0100

    Update main.cpp

--- classes/Tirette/src/main.cpp
+++ classes/Tirette/src/main.cpp
@@ -1,19 +1,23 @@
 #include <Arduino.h>
 #include "Tirette.hpp"
 
-int pin = 2;
+int pin = 3;
 Tirette tirette(pin);
+
 
 void setup(){
   Serial.begin(9600);
+  pinMode (3, INPUT_PULLUP);
 }
 
 void loop(){
   if (tirette.estEnPlace()){
     Serial.print("marche");
+    delay(10000);
   }
 
   else if (tirette.estRetiree()) {
     Serial.print("retire");
+    delay(10000);
   }
 }
